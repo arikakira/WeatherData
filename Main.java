b27108c4cda3304fa75678098c6bbd7e7cd013c8
arikakira <samanthal242@nycstudents.net>
2025-03-21 15:34:56 +0000

    Update

--- Main.java
+++ Main.java
@@ -3,14 +3,16 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         double[] temps = {
-            99.1, 142, 85, 86.1, 84.6, 
-            84.3, 124.9, 98, 101, 102.5
+            99.1, 142, 85, 85.1, 84.6, 
+            94.3, 124.9, 98, 101, 102.5
         };
         ArrayList<Double> t = new ArrayList<Double>();
         for(double n : temps) {
             t.add(n);
         }
         WeatherData w = new WeatherData(t);
+        w.cleanData(85.0, 120.0);       // test case 1?
+        System.out.println(w);
         t.add(1, 121.0);
         t.add(1, 84.0);
         w.cleanData(85.0, 120.0);
@@ -24,6 +26,6 @@ public class Main {
             newList.add(n);
         }
         w = new WeatherData(newList);
-        System.out.println(w. longestHeatWave(100.5));
+        System.out.println(w.longestHeatWave(100.5));       // test case 2?
     }
 }
